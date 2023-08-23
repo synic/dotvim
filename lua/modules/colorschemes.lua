f7a51c97bae2e3f31db114565453ac83ff3ffa79
Adam Olsen <arolsen@gmail.com>
2023-08-23 16:59:39 -0600

    more lazy loading configuration

---
+++ lua/modules/colorschemes.lua
@@ -0,0 +1,32 @@
+vim.keymap.set("n", "<space>tt", ":Telescope colorscheme<cr>")
+vim.g.gruvbox_material_background = "medium"
+vim.g.everforest_background = "hard"
+
+return {
+	{ "synic/jellybeans.vim", lazy = true },
+	{ "jnurmine/Zenburn", lazy = true },
+	{ "morhetz/gruvbox", lazy = true },
+	{ "synic/synic.vim", lazy = true },
+	{ "sonph/onehalf", lazy = true },
+	{
+		"sainnhe/gruvbox-material",
+		lazy = false,
+		config = function()
+			vim.cmd("colorscheme gruvbox-material")
+		end,
+	},
+	{ "NLKNguyen/papercolor-theme", lazy = true },
+	{ "sainnhe/everforest", lazy = true },
+	{ "gosukiwi/vim-atom-dark", lazy = true },
+	{ "tomasr/molokai", lazy = true },
+	{ "ray-x/aurora", lazy = true },
+	{ "sainnhe/sonokai", lazy = true },
+
+	{
+		"vim-airline/vim-airline-themes",
+		lazy = false,
+		config = function()
+			vim.g.airline_theme = "gruvbox_material"
+		end,
+	},
+}
