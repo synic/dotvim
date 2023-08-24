05aadda1bf9e2cefc3ea7d5b74ef950527437bb0
Adam Olsen <arolsen@gmail.com>
2023-08-24 11:02:29 -0600

    more lazy changes

--- lua/modules/language.lua
+++ lua/modules/language.lua
@@ -13,88 +13,83 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		run = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				rainbow = {
+		config = {
+			rainbow = { enable = true },
+			ensure_installed = {
+				"python",
+				"typescript",
+				"json",
+				"javascript",
+				"lua",
+				"yaml",
+				"query",
+				"sql",
+				"vim",
+				"bash",
+				"html",
+				"htmldjango",
+				"markdown",
+				"markdown_inline",
+				"regex",
+				"dart",
+				"go",
+			},
+			textobjects = {
+				select = {
 					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+					},
+					selection_modes = {
+						["@parameter.outer"] = "v", -- charwise
+						["@function.outer"] = "V", -- linewise
+						["@class.outer"] = "<c-v>", -- blockwise
+					},
+					include_surrounding_whitespace = true,
 				},
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true,
-						keymaps = {
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-							["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-						},
-						selection_modes = {
-							["@parameter.outer"] = "v", -- charwise
-							["@function.outer"] = "V", -- linewise
-							["@class.outer"] = "<c-v>", -- blockwise
-						},
-						include_surrounding_whitespace = true,
+				swap = {
+					enable = true,
+					swap_next = {
+						["<leader>a"] = "@parameter.inner",
 					},
-					swap = {
-						enable = true,
-						swap_next = {
-							["<leader>a"] = "@parameter.inner",
-						},
-						swap_previous = {
-							["<leader>A"] = "@parameter.inner",
-						},
-					},
-					move = {
-						enable = true,
-						set_jumps = true, -- whether to set jumps in the jumplist
-						goto_next_start = {
-							["]m"] = "@function.outer",
-							["]]"] = { query = "@class.outer", desc = "Next class start" },
-						},
-						goto_next_end = {
-							["]M"] = "@function.outer",
-							["]["] = "@class.outer",
-						},
-						goto_previous_start = {
-							["[m"] = "@function.outer",
-							["[["] = "@class.outer",
-						},
-						goto_previous_end = {
-							["[M"] = "@function.outer",
-							["[]"] = "@class.outer",
-						},
-					},
-					lsp_interop = {
-						enable = true,
-						border = "none",
-						peek_definition_code = {
-							["<leader>df"] = "@function.outer",
-							["<leader>dF"] = "@class.outer",
-						},
+					swap_previous = {
+						["<leader>A"] = "@parameter.inner",
 					},
 				},
-			})
-
-			local ts_parsers = require("nvim-treesitter.parsers")
-
-			vim.api.nvim_create_autocmd("BufEnter", {
-				pattern = { "*" },
-				callback = function()
-					local ft = vim.bo.filetype
-					if not ft then
-						return
-					end
-					local parser = ts_parsers.filetype_to_parsername[ft]
-					if not parser then
-						return
-					end
-					local is_installed = ts_parsers.has_parser(ts_parsers.ft_to_lang(ft))
-					if not is_installed then
-						vim.cmd("TSInstall " .. parser)
-					end
-				end,
-			})
-		end,
+				move = {
+					enable = true,
+					set_jumps = true, -- whether to set jumps in the jumplist
+					goto_next_start = {
+						["]m"] = "@function.outer",
+						["]]"] = { query = "@class.outer", desc = "Next class start" },
+					},
+					goto_next_end = {
+						["]M"] = "@function.outer",
+						["]["] = "@class.outer",
+					},
+					goto_previous_start = {
+						["[m"] = "@function.outer",
+						["[["] = "@class.outer",
+					},
+					goto_previous_end = {
+						["[M"] = "@function.outer",
+						["[]"] = "@class.outer",
+					},
+				},
+				lsp_interop = {
+					enable = true,
+					border = "none",
+					peek_definition_code = {
+						["<leader>df"] = "@function.outer",
+						["<leader>dF"] = "@class.outer",
+					},
+				},
+			},
+		},
 	},
 
 	"nvim-treesitter/nvim-treesitter-textobjects",
