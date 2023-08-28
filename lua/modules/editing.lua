8693f87b4273b8c751050ca15d77ca6a280b2444
Adam Olsen <arolsen@gmail.com>
2023-08-28 16:32:22 -0600

    move luasnippet to telescope

--- lua/modules/editing.lua
+++ lua/modules/editing.lua
@@ -45,10 +45,13 @@ return {
 	-- snippets
 	{
 		"L3MON4D3/LuaSnip",
+
+		dependencies = { "rafamadriz/friendly-snippets" },
 		config = function()
 			require("luasnip.loaders.from_snipmate").lazy_load({
 				paths = { "~/.config/nvim/snippets/" },
 			})
+			require("luasnip.loaders.from_vscode").lazy_load()
 		end,
 	},
 }
