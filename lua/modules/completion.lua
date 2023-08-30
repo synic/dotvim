0804b0fe4a613d68d7340ea9678f8ad3405175e4
Adam Olsen <arolsen@gmail.com>
2023-08-30 15:45:31 -0600

    fix luasnip completion

--- lua/modules/completion.lua
+++ lua/modules/completion.lua
@@ -7,9 +7,15 @@ return {
 		lazy = false,
 		config = function()
 			local cmp = require("cmp")
+			local luasnip = require("luasnip")
 
 			local has_words_before = function()
-				local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+				local cursor = vim.api.nvim_win_get_cursor(0)
+				if not cursor then
+					return
+				end
+
+				local line, col = table.unpack(cursor)
 				return col ~= 0
 					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 			end
@@ -29,6 +35,8 @@ return {
 					["<Tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
 						elseif has_words_before() then
 							cmp.complete()
 						else
@@ -39,6 +47,8 @@ return {
 					["<S-Tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
 						else
 							fallback()
 						end
--- lua/modules/editing.lua
+++ lua/modules/editing.lua
@@ -49,7 +49,7 @@ return {
 		dependencies = { "rafamadriz/friendly-snippets" },
 		config = function()
 			require("luasnip.loaders.from_snipmate").lazy_load({
-				paths = { "~/.config/nvim/snippets/" },
+				paths = { vim.fn.stdpath("config") .. "/snippets" },
 			})
 			require("luasnip.loaders.from_vscode").lazy_load()
 		end,
