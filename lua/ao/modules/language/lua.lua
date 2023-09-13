1cc797340b37a8a54edc28c89cc5106a55aa3410
Adam Olsen <arolsen@gmail.com>
2023-09-13 17:25:55 -0600

    still trying to fix lua lsp

--- lua/ao/modules/language/lua.lua
+++ lua/ao/modules/language/lua.lua
@@ -21,6 +21,9 @@ return {
             diagnostics = {
               globals = { "vim", "hs" },
             },
+            completion = {
+              callSnippet = "Replace",
+            },
           },
         },
       })
