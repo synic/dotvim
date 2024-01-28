50a22123d7b881d58bb0c6c095731e64e93a1e94
Adam Olsen <arolsen@gmail.com>
2024-01-28 16:11:46 -0700

    switch from lir.nvim to oil.nvim

--- lua/ao/modules/filesystem.lua
+++ lua/ao/modules/filesystem.lua
@@ -31,42 +31,16 @@ vim.g.netrw_banner = 0
 vim.g.netrw_list_hide = (vim.fn["netrw_gitignore#Hide"]()) .. [[,\(^\|\s\s\)\zs\.\S\+]]
 vim.g.netrw_browse_split = 0
 
-local function delete()
-  local lvim = require("lir.vim")
-  local actions = require("lir.actions")
-  local path = require("plenary.path")
-  local ctx = lvim.get_context()
-
-  local items = ctx:get_marked_items()
-  local count = #items
-
-  if #items == 0 then
-    actions.delete()
-  else
-    if vim.fn.confirm("Delete " .. count .. " items?", "&Yes\n&No", 1) ~= 1 then
-      return
-    end
-
-    for _, item in pairs(items) do
-      local p = path:new(item.fullpath)
-      if p:is_dir() then
-        p:rm({ recursive = true })
-      else
-        if not vim.loop.fs_unlink(p:absolute()) then
-          utils.error("Delete file failed")
-          return
-        end
-      end
-    end
-
-    actions.reload()
+local function change_to_project_root()
+  local r = utils.find_project_root()
+  if r then
+    vim.cmd.cd(r)
   end
 end
 
 local function touch()
-  local actions = require("lir.actions")
+  local oil = require("oil")
   local path = require("plenary.path")
-  local lvim = require("lir.vim")
 
   vim.ui.input({ prompt = "Create file (or directory): " }, function(name)
     if name == nil then
@@ -74,19 +48,18 @@ local function touch()
     end
 
     if name == "." or name == ".." then
-      utils.error("Invalid file name: " .. name)
+      print("Invalid file name: " .. name)
       return
     end
 
-    local ctx = lvim.get_context()
-    local p = path:new(ctx.dir .. name)
+    local dir = oil.get_current_dir()
+    if not dir then
+      return
+    end
+
+    local p = path:new(dir .. name)
     if p:exists() then
-      utils.error("File already exists")
-      -- cursor jump
-      local lnum = ctx:indexof(name)
-      if lnum then
-        vim.cmd(tostring(lnum))
-      end
+      print("File already exists: ", path.filename)
       return
     end
 
@@ -96,92 +69,165 @@ local function touch()
       p:touch()
     end
 
-    actions.reload()
-
-    vim.schedule(function()
-      local lnum = lvim.get_context():indexof(name)
-      if lnum then
-        vim.cmd(tostring(lnum))
-      end
-    end)
+    vim.cmd.edit({ bang = true })
   end)
 end
 
 return {
-  -- fancy replacement for netrw, with devicons
   {
-    "tamago324/lir.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-      local actions = require("lir.actions")
-      local mark_actions = require("lir.mark.actions")
-      local clipboard_actions = require("lir.clipboard.actions")
+    "stevearc/oil.nvim",
+    opts = function()
+      local oil = require("oil")
+      local actions = require("oil.actions")
 
-      require("lir").setup({
-        show_hidden_files = true,
-        devicons = { enable = true },
-        ignore = { ".mypy_cache", ".git", ".tmp", "node_modules", "*_templ.go", ".DS_Store" },
-        mappings = {
-          ["l"] = actions.edit,
-          ["<return>"] = actions.edit,
-          ["<C-s>"] = actions.split,
-          ["<C-v>"] = actions.vsplit,
-          ["g/"] = actions.vsplit,
-          ["g-"] = actions.split,
-          ["<C-t>"] = actions.tabedit,
-
-          ["h"] = actions.up,
-          ["q"] = actions.quit,
-          ["<esc>"] = actions.quit,
-
-          ["gk"] = actions.mkdir,
-          ["gn"] = touch,
-          ["gr"] = actions.rename,
-          ["gR"] = actions.reload,
-          ["gy"] = actions.yank_path,
-          ["g."] = actions.toggle_show_hidden,
-          ["gd"] = delete,
-          ["gD"] = actions.wipeout,
-
-          ["gm"] = function()
-            mark_actions.toggle_mark()
-            vim.cmd("normal! j")
-          end,
-          ["gc"] = clipboard_actions.copy,
-          ["gx"] = clipboard_actions.cut,
-          ["gp"] = clipboard_actions.paste,
+      return {
+        columns = {
+          "permissions",
+          "size",
+          "mtime",
+          { "icon", add_padding = false },
         },
-        float = {
-          winblend = 0,
-          curdir_window = {
-            enable = false,
-            highlight_dirname = false,
+        skip_confirm_for_simple_edits = true,
+        lsp_rename_autosave = true,
+        add_padding = false,
+        keymaps = {
+          ["<CR>"] = {
+            desc = "Select",
+            callback = function()
+              actions.select.callback(nil, change_to_project_root)
+            end,
+          },
+          ["g/"] = {
+            desc = "Open in vertical split",
+            callback = function()
+              oil.select({ vertical = true }, change_to_project_root)
+            end,
+          },
+          ["g-"] = {
+            desc = "Open in horizontal split",
+            callback = function()
+              oil.select({ horizontal = true }, change_to_project_root)
+            end,
+          },
+          ["gt"] = {
+            desc = "Open in new tab",
+            callback = function()
+              oil.select({ tab = true }, change_to_project_root)
+            end,
+          },
+          ["<C-p>"] = "actions.preview",
+          ["<C-c>"] = "actions.close",
+          ["<C-l>"] = "actions.refresh",
+          ["gs"] = "actions.change_sort",
+          ["gx"] = "actions.open_external",
+          ["gn"] = {
+            desc = "Create new file",
+            callback = function()
+              touch()
+            end,
+          },
+          ["g\\"] = "actions.toggle_trash",
+          ["g."] = "actions.toggle_hidden",
+          ["h"] = {
+            desc = "parent",
+            callback = function()
+              require("oil.actions").parent.callback()
+              change_to_project_root()
+            end,
+          },
+          ["l"] = {
+            desc = "select",
+            callback = function()
+              require("oil.actions").select.callback(nil, change_to_project_root)
+            end,
           },
         },
-        hide_cursor = true,
-      })
-
-      vim.api.nvim_create_autocmd({ "FileType" }, {
-        pattern = { "lir" },
-        callback = function()
-          -- use visual mode
-          vim.api.nvim_buf_set_keymap(
-            0,
-            "x",
-            "m",
-            ':<C-u>lua require"lir.mark.actions".toggle_mark("v")<CR>',
-            { noremap = true, silent = true }
-          )
-
-          -- echo cwd
-          vim.api.nvim_echo({ { vim.fn.expand("%:p"), "Normal" } }, false, {})
-        end,
+        use_default_keymaps = true,
+      }
+    end,
+    dependencies = { "nvim-tree/nvim-web-devicons", "ahmedkhalf/project.nvim" },
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "oil",
+        callback = change_to_project_root,
       })
     end,
   },
+
+  -- fancy replacement for netrw, with devicons
+  -- {
+  --   "tamago324/lir.nvim",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-tree/nvim-web-devicons",
+  --   },
+  --   config = function()
+  --     local actions = require("lir.actions")
+  --     local mark_actions = require("lir.mark.actions")
+  --     local clipboard_actions = require("lir.clipboard.actions")
+  --
+  --     require("lir").setup({
+  --       show_hidden_files = true,
+  --       devicons = { enable = true },
+  --       ignore = { ".mypy_cache", ".git", ".tmp", "node_modules", "*_templ.go", ".DS_Store" },
+  --       mappings = {
+  --         ["l"] = actions.edit,
+  --         ["<return>"] = actions.edit,
+  --         ["<C-s>"] = actions.split,
+  --         ["<C-v>"] = actions.vsplit,
+  --         ["g/"] = actions.vsplit,
+  --         ["g-"] = actions.split,
+  --         ["<C-t>"] = actions.tabedit,
+  --
+  --         ["h"] = actions.up,
+  --         ["q"] = actions.quit,
+  --         ["<esc>"] = actions.quit,
+  --
+  --         ["gk"] = actions.mkdir,
+  --         ["gn"] = touch,
+  --         ["gr"] = actions.rename,
+  --         ["gR"] = actions.reload,
+  --         ["gy"] = actions.yank_path,
+  --         ["g."] = actions.toggle_show_hidden,
+  --         ["gd"] = delete,
+  --         ["gD"] = actions.wipeout,
+  --
+  --         ["gm"] = function()
+  --           mark_actions.toggle_mark()
+  --           vim.cmd("normal! j")
+  --         end,
+  --         ["gc"] = clipboard_actions.copy,
+  --         ["gx"] = clipboard_actions.cut,
+  --         ["gp"] = clipboard_actions.paste,
+  --       },
+  --       float = {
+  --         winblend = 0,
+  --         curdir_window = {
+  --           enable = false,
+  --           highlight_dirname = false,
+  --         },
+  --       },
+  --       hide_cursor = true,
+  --     })
+  --
+  --     vim.api.nvim_create_autocmd({ "FileType" }, {
+  --       pattern = { "lir" },
+  --       callback = function()
+  --         -- use visual mode
+  --         vim.api.nvim_buf_set_keymap(
+  --           0,
+  --           "x",
+  --           "m",
+  --           ':<C-u>lua require"lir.mark.actions".toggle_mark("v")<CR>',
+  --           { noremap = true, silent = true }
+  --         )
+  --
+  --         -- echo cwd
+  --         vim.api.nvim_echo({ { vim.fn.expand("%:p"), "Normal" } }, false, {})
+  --       end,
+  --     })
+  --   end,
+  -- },
   {
     "nvim-tree/nvim-tree.lua",
     version = "*",
